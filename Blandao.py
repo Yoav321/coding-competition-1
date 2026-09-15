af3c65413dc6f47a7c767faf9fabd2b7ffd7ebee
Yoav321 <yoav.bierkatz@gmail.com>
2026-09-15 11:30:10 -0600

    attempt blandao

---
+++ Blandao.py
@@ -0,0 +1,25 @@
+"""
+
+Yoav Bierkatz - September 2026
+"""
+
+def main() -> None:
+  n: int = int(input(""))
+  kjuklingur: str
+  nautakjot: str
+
+
+  for options in range(1, n+1):
+    meat = input("")
+
+  if meat == kjuklingur and nautakjot:
+    print("blandad best")
+  elif meat == kjuklingur:
+    print("kjuklingur")
+  else:
+    print("nautakjot")
+
+
+if __name__ == "__main__":
+  main()
+    
